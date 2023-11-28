7db9bb6bb38f6461b9c99061430ee9e6a7ad81cf
Cristian B <cris.bahena77@gmail.com>
2023-11-28 13:36:51 -0600

    Added reader/writer

--- store_item.rb
+++ store_item.rb
@@ -1,21 +1,21 @@
 # Use hashes with symbols to represent the following scenario:
 # C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
 
-myItems = {brand: "Lego", color: "Red", price: 20}
-pp myItems
+# myItems = {brand: "Lego", color: "Red", price: 20}
+# pp myItems
 
 # C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
 
-itemOne = {brand: "Lego", color: "Red", price: 20}
-itemTwo = {brand: "Nike", color: "Brown", price: 200}
-itemThree = {brand: "Adidas", color: "Yellow", price: 250}
-pp "One #{itemOne}, Two #{itemTwo}, Three #{itemThree}"
+# itemOne = {brand: "Lego", color: "Red", price: 20}
+# itemTwo = {brand: "Nike", color: "Brown", price: 200}
+# itemThree = {brand: "Adidas", color: "Yellow", price: 250}
+# pp "One #{itemOne}, Two #{itemTwo}, Three #{itemThree}"
 
 # C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
-itemOne = {"brand" => "Lego", "color" => "Red", "price"=> 20}
-itemTwo = {"brand" => "Nike", "color"=> "Brown", "price"=> 200}
-itemThree = {"brand" => "Adidas", "color"=> "Yellow", "price"=> 250}
-pp "One #{itemOne}, Two #{itemTwo}, Three #{itemThree}"
+# itemOne = {"brand" => "Lego", "color" => "Red", "price"=> 20}
+# itemTwo = {"brand" => "Nike", "color"=> "Brown", "price"=> 200}
+# itemThree = {"brand" => "Adidas", "color"=> "Yellow", "price"=> 250}
+# pp "One #{itemOne}, Two #{itemTwo}, Three #{itemThree}"
 
 # Bonus: Read more about Ruby array and hash methods. Explore using different methods in your code.
 # https://ruby-doc.org/core/Array.html
@@ -33,7 +33,15 @@ class StoreItem
   end
 
   def print_info
-    pp "The item brand is #{@brand}, it is #{@color}, and costs $#{@price}."
+    puts "The item brand is #{@brand}, it is #{@color}, and costs $#{@price}."
+  end
+
+  def read
+    attr_reader :price?
+  end
+
+  def write
+    attr_writer :brand, :color?
   end
 end
 
