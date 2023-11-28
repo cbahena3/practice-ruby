399748c91379614a96e7d8f494b3f219bdc7a0c5
Cristian B <cris.bahena77@gmail.com>
2023-11-28 12:48:26 -0600

    Added class

--- store_item.rb
+++ store_item.rb
@@ -1,15 +1,15 @@
 # Use hashes with symbols to represent the following scenario:
 # C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
 
-# myItems = {brand: "Lego", color: "Red", price: 20}
-# pp myItems
+myItems = {brand: "Lego", color: "Red", price: 20}
+pp myItems
 
 # C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
 
-# itemOne = {brand: "Lego", color: "Red", price: 20}
-# itemTwo = {brand: "Nike", color: "Brown", price: 200}
-# itemThree = {brand: "Adidas", color: "Yellow", price: 250}
-# pp "One #{itemOne}, Two #{itemTwo}, Three #{itemThree}"
+itemOne = {brand: "Lego", color: "Red", price: 20}
+itemTwo = {brand: "Nike", color: "Brown", price: 200}
+itemThree = {brand: "Adidas", color: "Yellow", price: 250}
+pp "One #{itemOne}, Two #{itemTwo}, Three #{itemThree}"
 
 # C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
 itemOne = {"brand" => "Lego", "color" => "Red", "price"=> 20}
@@ -24,3 +24,22 @@ pp "One #{itemOne}, Two #{itemTwo}, Three #{itemThree}"
 
 # Bonus:
 # Try to represent the same store items using a class!
+
+class StoreItem
+  def initialize (brand, color, price)
+    @brand = brand
+    @color = color
+    @price = price
+  end
+
+  def print_info
+    pp "The item brand is #{@brand}, it is #{@color}, and costs $#{@price}."
+  end
+end
+
+itemOne = StoreItem.new("Lego", "Red", 20)
+itemTwo = StoreItem.new("Nike", "Brown", 200)
+itemThree = StoreItem.new("Adidas", "Yellow", 250)
+itemOne.print_info
+itemTwo.print_info
+itemThree.print_info
