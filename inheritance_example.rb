e75ed03f9a29015f2252bf1351ff7f21eb818214
Cristian B <cris.bahena77@gmail.com>
2023-11-29 11:47:09 -0600

    added new method

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Car
+class Transportation
   def initialize
     @speed = 0
     @direction = 'north'
@@ -15,15 +15,30 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+
+
+class Car < Transportation
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike < Car
-
+class Bike < Transportation
   def ring_bell
     puts "Ring ring!"
   end
+
+  def accelerate
+    @speed += 5
+  end
 end
+
+bike = Bike.new()
+bike.ring_bell
+pp bike.accelerate
+
+car = Car.new()
+car.honk_horn
+pp car.accelerate
